c84713d60b6cdeca4a7262cae31926fcd0a2b6f4
Tin Wan Ying <wanying@squarrific.com>
2018-07-10 21:40:46 +1200

    first commit ORZ

---
+++ app/components/page-component.js
@@ -0,0 +1,1 @@
+export { default } from 'theme-beagle/components/page-component';
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: 'theme-beagle'
+  name: 'theme-beagle',
+
+  included: function(app) {
+    app.import('vendor/beagle-v1.4.2/BS4/dist/starter/assets/lib/material-design-icons/fonts/Material-Design-Iconic-Font.eot', { destDir: 'fonts' });
+    app.import('vendor/beagle-v1.4.2/BS4/dist/starter/assets/lib/material-design-icons/fonts/Material-Design-Iconic-Font.svg', { destDir: 'fonts' });
+    app.import('vendor/beagle-v1.4.2/BS4/dist/starter/assets/lib/material-design-icons/fonts/Material-Design-Iconic-Font.ttf', { destDir: 'fonts' });
+    app.import('vendor/beagle-v1.4.2/BS4/dist/starter/assets/lib/material-design-icons/fonts/Material-Design-Iconic-Font.woff', { destDir: 'fonts' });
+    app.import('vendor/beagle-v1.4.2/BS4/dist/starter/assets/lib/material-design-icons/fonts/Material-Design-Iconic-Font.woff2', { destDir: 'fonts' });
+  },
 };
